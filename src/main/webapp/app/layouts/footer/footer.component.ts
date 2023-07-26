f66179401cfe3b97f9bd4b961f25a9b5e3576d3f
hoangdp <kunboy1608@gmail.com>
2023-07-26 10:30:03 +0700

    update footer and navbar

--- src/main/webapp/app/layouts/footer/footer.component.ts
+++ src/main/webapp/app/layouts/footer/footer.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import SharedModule from 'app/shared/shared.module';
 
 @Component({
   standalone: true,
   selector: 'jhi-footer',
   templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss'],
+  imports: [SharedModule]
 })
-export default class FooterComponent {}
+export default class FooterComponent {
+  openLink(url: string) {
+    window.open(url, '_blank')
+  }
+}
